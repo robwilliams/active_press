5c36e9deaeb05faec3156aec418235aefe4bd675
robwilliams <rob@r-williams.com>
2011-08-22 12:09:28 +0100

    Even more associations

--- lib/active_press/models/post.rb
+++ lib/active_press/models/post.rb
@@ -3,4 +3,5 @@ class ActivePress::Post < ActivePress::Base
   set_primary_key "ID"
   
   has_many :comments, :foreign_key => "comment_post_ID"
+  belongs_to :user, :foreign_key => "post_author"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe ActivePress::Post do
+describe ActivePress::User do
+  it { should have_many(:posts) }
   it { should have_many(:comments) }
-  it { should belong_to(:user) }
 end
