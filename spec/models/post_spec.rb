4b0c31e96c90e70b780aeb9930c99d2438b5052b
Robert Williams <rob@r-williams.com>
2011-08-27 02:00:32 +0100

    Pending scope specs that are required

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,4 +8,22 @@ describe ActivePress::Post do
   it { should have_many(:term_taxonomies).through(:term_relationships) }
   it { should have_many(:categories).through(:term_relationships) }
   it { should have_many(:tags).through(:term_relationships) }
+  
+  context :scope do
+    describe :by_post_status do
+      subject { by_post_status('draft') }
+      it "should only return posts that have the same post_status"
+    end
+    
+    describe :by_post_type do
+      subject { by_post_type('post') }
+      it "should only return posts that have the same post_type"
+    end
+    
+    describe :published do
+      subject { published }
+      it "should only return posts that have a post_status of 'publish'"
+      it "should only return posts that have a post_date before now"
+    end    
+  end
 end
